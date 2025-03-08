2b2119e2fae980bff22cf69ef1b2d855be0266ef
Enchong <deveraflorencio18@gmail.com>
2025-03-08 23:45:34 +0800

    wip

--- resources/js/swagger.js
+++ resources/js/swagger.js
@@ -1,4 +1,5 @@
-import SwaggerUI from "swagger-ui-dist/swagger-ui-es-bundle.js";
+// import SwaggerUI from "swagger-ui-dist/swagger-ui-es-bundle.js";
+import SwaggerUI from "swagger-ui-dist/swagger-ui-bundle.js";
 import "swagger-ui-dist/swagger-ui.css";
 
 SwaggerUI({
--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,23 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/swagger.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/js/swagger.js'
+            ],
             refresh: true,
         }),
         tailwindcss(),
     ],
+    resolve: {
+        alias: {
+            "swagger-ui-dist": "/node_modules/swagger-ui-dist", // Ensure correct path resolution
+        },
+    },
     build: {
         rollupOptions: {
             external: ['swagger-ui-dist'],
-        }
-    }
+        },
+    },
 });
