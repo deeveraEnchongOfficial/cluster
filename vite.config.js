c47165e66af29cfd3034c2f04ea2d6f35a0047aa
Enchong <deveraflorencio18@gmail.com>
2025-03-08 23:28:56 +0800

    - configuring

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-                'resources/js/swagger.js'
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/swagger.js'],
             refresh: true,
         }),
         tailwindcss(),
     ],
+    build: {
+        rollupOptions: {
+            external: ['swagger-ui-dist'],
+        }
+    }
 });
